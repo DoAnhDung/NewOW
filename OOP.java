16954729794152de2de848d0388e24f38ba8ba3b
DoAnhDung <77825151+DoAnhDung@users.noreply.github.com>
2022-08-23 04:16:56 +0700

    Add files via upload

---
+++ OOP.java
@@ -0,0 +1,45 @@
+public class OOP {
+    // khai báo các thuộc tính
+    public String hoTen;
+    public int tuoi;
+    public String diaChi;
+    public String soDienThoai;
+
+    // khai báo cac phương thức
+    public void an(){
+        System.out.println(" an ");
+    }
+
+    public void ngu(){
+        System.out.println(" ngu ");
+    }
+
+}
+
+// BT: tạo 1 class hình chữ nhật có 2 thuộc tính: dài, rộng
+                                 // có 3 phương thức, tính chu vi, diện tích, toString,
+                                // Có 2 constructor: 0 có tham số và có đầy đủ tham số
+// Khởi tạo 3 hình chữ nhật tính chu vi diện tích của 3 hình chữ nhật.
+
+
+
+/*- 1 phong cách lập trình đưa nhũng đối tượng thực tế vào lập trình
+* - thể hiện qua class và object
+* - class: do người dùng tự định nghĩa / đại diện thể hiện cho 1 tập hợp các đối tượng có thông tin tương tự nhau
+* - object: thuộc 1 class => class chứa thuộc tính và phương thức của object
+*
+* Cách khai báo/ định nghĩa class:
+*       <access modifier> - < tên class > {
+*              thuộc tính;
+*              Phương thức; }
+*
+* <tên class> <tên đối tượng> = new <tên class>();
+*
+* truy xuất thuộc tính của object
+* < tên đối tương >. <tên thuộc tính>
+  < tên đối tượng >. < tên method>(tham số)
+*
+*  */
+
+// Format assignment: Font: Time New Roman
+//                    Size: 12 giãn dòng : 1,5 Lề trái 2,5 phải dưới trên 2cm.
